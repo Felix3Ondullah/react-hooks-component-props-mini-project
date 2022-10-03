faa8a6d0ffaad0e299f0862d4e01394199484920
Felix Ondullah <felixondullah@gmail.com>
2022-10-03 19:12:16 +0300

    test passed

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-
-function About ( { image = "https://via.placeholder.com/215", about } )
-{
-  return (
+const About = ({image = "https://via.placeholder.com/215", about}) => (
     <aside>
-          <img src={ image } alt="blog logo"></img>
-          
-          <p>{ about }</p>
-          
+        <img src={image} alt="blog logo"/>
+        <p>{about}</p>
     </aside>
-  );
-}
+)
+
 export default About;
